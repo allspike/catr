b8d3515236b67d79b18a424e5d4c9409da079262
allspike <jspikerman@icloud.com>
2025-08-09 16:01:03 -0700

    Added test folder as well as test inputs/outputs

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,16 @@
+use clap::Parser;
+
+#[derive(Parser, Debug)]
+#[command(author, version, about)]
+/// A Rust version of the 'cat' command
+struct Args {
+    /// The file to read from
+    #[arg(required(true))]
+    file: String,
+
+    #[arg(short('b'))]
+    non_blank_lines: bool,
+}
 fn main() {
-    println!("Hello, world!");
+    return;
 }
